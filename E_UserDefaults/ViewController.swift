5e8a3c0a0642748b48d94bf1c53f919613422721
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-06-06 07:43:09 -0700

    Initial Commit

---
+++ E_UserDefaults/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  E_UserDefaults
+//
+//  Created by Aplicaciones Moviles on 6/06/15.
+//  Copyright (c) 2015 MobileLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
